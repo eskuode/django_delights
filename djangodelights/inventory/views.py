43ee393ab11f0e2c4814bc1a6987a38680f83131
eskuode <eramanenko@gmail.com>
2023-10-18 13:02:06 +0100

    added get_context_data function

--- djangodelights/inventory/views.py
+++ djangodelights/inventory/views.py
@@ -3,6 +3,17 @@ from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
 from django.views.generic import ListView
 
 # Create your views here.
+class HomeView(TemplateView):
+  template_name = "home.html"
+
+  def get_context_data(self):
+    context = super().get_context_data()
+    context["ingredients"] = Ingredient.objects.all()
+    context["menu"] = MenuItem.objects.all()
+    context["recipes"] = RecipeRequirement.objects.all()
+    context["purchases"] = Purchase.objects.all()
+    
+    return context
 class IngredientView(ListView):
     model = Ingredient
     template = "inventory/ingredient.html"
