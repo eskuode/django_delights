5019aa49e56b67adcdfe45f573b9eb29b27181e9
eskuode <eramanenko@gmail.com>
2023-10-18 13:28:01 +0100

    build base and home html

--- djangodelights/djangodelights/urls.py
+++ djangodelights/djangodelights/urls.py
@@ -20,6 +20,7 @@ from inventory import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomeView.as_view(), name='home'),
     path('ingredient/list', views.IngredientView.as_view(), name="ingredients"),
     path('menu/list', views.MenuView.as_view(), name="menu"),
     path('recipe/list', views.RecipeView.as_view(), name="recipes"),
