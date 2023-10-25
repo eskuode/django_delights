3b1b7e5faaf508501f5006c591865ac6645d74b7
eskuode <eramanenko@gmail.com>
2023-10-25 12:40:49 +0100

    created url paths for CreateView classes

--- djangodelights/inventory/urls.py
+++ djangodelights/inventory/urls.py
@@ -5,7 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('ingredients/list', views.IngredientView.as_view(), name="ingredients"),
+    path('ingredients/create', views.IngredientCreate.as_view(), name="ingredientcreate"),
     path('menu/list', views.MenuView.as_view(), name='menu'),
+    path('menu/create', views.MenuCreate.as_view(), name="menucreate"),
+    path('recipes/create', views.RecipeCreate.as_view(), name="recipecreate"),
     path('purchases/list', views.PurchaseView.as_view(), name="purchases"),
+    path('purchase/create', views.PurchaseView.as_view(), name="purchasecreate"),
     path('profit/list', views.ProfitView.as_view(), name='profit'),
 ]
