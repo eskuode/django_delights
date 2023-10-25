3611d156d9fb8fba346317bc79b82d3365800777
eskuode <eramanenko@gmail.com>
2023-10-25 12:48:14 +0100

    added verbose names to models and get absolute url method

--- djangodelights/inventory/models.py
+++ djangodelights/inventory/models.py
@@ -15,33 +15,41 @@ class Ingredient(models.Model):
     (EGGS, "eggs"),
     (OTHER, "other")
   ]
-  name = models.CharField(max_length=50)
-  quantity = models.FloatField(default=0.0)
-  unit = models.CharField(max_length=2, choices=UNIT_TYPE_CHOICES, default=OTHER)
-  unit_price = models.FloatField(default=0.0)
+  name = models.CharField(max_length=50, verbose_name= "Ingredient")
+  quantity = models.FloatField(default=0.0, verbose_name="Quantity")
+  unit = models.CharField(max_length=2, choices=UNIT_TYPE_CHOICES, default=OTHER, verbose_name="Units")
+  unit_price = models.FloatField(default=0.0, verbose_name="Price per unit")
   def __str__(self):
     return self.name +"-"+ str(self.quantity) +" "+ str(self.unit_price)+"/"+self.unit
-
+  def get_absolute_url(self):
+    return "/inventory/"
+  
 class MenuItem(models.Model):
-  title = models.CharField(max_length=50)
-  price = models.FloatField(default=0.0)
+  title = models.CharField(max_length=50, verbose_name="Menu Item")
+  price = models.FloatField(default=0.0, verbose_name="Price")
   def __str__(self):
     return self.title + " " + str(self.price)
+  def get_absolute_url(self):
+    return "/inventory/"
   
 class RecipeRequirement(models.Model):
-  menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-  ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-  quantity = models.FloatField(default=0.0)
+  menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE, verbose_name="Menu Item")
+  ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, verbose_name="Ingrediet")
+  quantity = models.FloatField(default=0.0, verbose_name="Quantity")
   def __str__(self):
     return self.menu_item.title + ": " + self.ingredient.name + "/" + str(self.quantity)
   @property
   def cost(self):
     return self.quantity * self.ingredient.unit_price
+  def get_absolute_url(self):
+    return "/inventory/"
 
 
 class Purchase(models.Model):
-  menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-  timestamp = models.DateTimeField(default=datetime.now(), blank=True)
+  menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE, verbose_name="Menu Item")
+  timestamp = models.DateTimeField(default=datetime.now(), blank=True, verbose_name="Order Time")
   def __str__(self):
     return self.menu_item.title + " " + str(self.timestamp)
+  def get_absolute_url(self):
+    return "/inventory/"
 # Create your models here.
