6fcdfcf82a829dd0e4d9d38d770a104616786443
eskuode <eramanenko@gmail.com>
2023-10-25 12:30:56 +0100

    imported forms into views and created createview classes

--- djangodelights/inventory/forms.py
+++ djangodelights/inventory/forms.py
@@ -2,17 +2,21 @@ from django import forms
 from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
 
 class IngredientCreateForm(forms.ModelForm):
-    model = Ingredient
-    fields = "__all__"
+    class Meta:
+        model = Ingredient
+        fields = "__all__"
 
 class MenuItemCreateForm(forms.ModelForm):
-    model = MenuItem
-    fields = "__all__"
+    class Meta:
+        model = MenuItem
+        fields = "__all__"
 
 class RecipeRequirementCreateForm(forms.ModelForm):
-    model = RecipeRequirement
-    fields = "__all__"
+    class Meta:
+        model = RecipeRequirement
+        fields = "__all__"
 
 class PurchaseCreateForm(forms.ModelForm):
-    model = Purchase
-    fields = "__all__"
+    class Meta:
+        model = Purchase
+        fields = "__all__"
--- djangodelights/inventory/views.py
+++ djangodelights/inventory/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
+from .forms import IngredientCreateForm, MenuItemCreateForm, RecipeRequirementCreateForm, PurchaseCreateForm
 from django.views.generic import TemplateView, ListView
+from django.views.generic.edit import CreateView
 from django.db.models import Sum
 
 # Create your views here.
@@ -24,6 +26,11 @@ class IngredientView(ListView):
        context["ingredients"] = Ingredient.objects.all()
        return context
     
+class IngredientCreate(CreateView):
+    model = Ingredient
+    form_class = IngredientCreateForm
+    template_name = "inventory/add_ingredient.html"
+    
 class MenuView(ListView):
     model = MenuItem
     template_name = "inventory/menu.html"
@@ -31,6 +38,11 @@ class MenuView(ListView):
        context = super().get_context_data()
        context["menu"] = MenuItem.objects.all()   
        return context
+    
+class MenuCreate(CreateView):
+    model = MenuItem
+    form_class = MenuItemCreateForm
+    template_name = "inventory/add_menu.html"
 
 class RecipeView(ListView):
     model = RecipeRequirement
@@ -41,10 +53,12 @@ class RecipeView(ListView):
         context["menu"] = MenuItem.objects.all()
         context["recipes"] = RecipeRequirement.objects.all()
         return context
-
-  
-        
     
+class RecipeCreate(CreateView):
+    model = RecipeRequirement
+    form_class = RecipeRequirementCreateForm
+    template_name = "inventory/add_recipe.html"
+
     
 class PurchaseView(ListView):
     model = Purchase
@@ -54,6 +68,11 @@ class PurchaseView(ListView):
         context["purchases"] = Purchase.objects.all()
         return context
    
+class PurchaseCreate(CreateView):
+    model = Purchase
+    form_class = PurchaseCreateForm
+    template_name = "inventory/add_purchase.html"
+
 class ProfitView(TemplateView):
     template_name = "inventory/profit.html"
     def get_context_data(self, **kwargs):
